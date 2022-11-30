22006efc3bf03c09e04ce44ded671ea79ae8fd53
Shabina Singh <anibahs@gmail.com>
2022-11-29 21:00:17 -0500

    intializing consumer banking

---
+++ src/com/bms/model/ConsumerBanking.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bms.model;
+
+/**
+ *
+ * @author Shabina
+ */
+public class ConsumerBanking {
+    private int consumerId;
+
+    public int getConsumerId() {
+        return consumerId;
+    }
+
+    public void setConsumerId(int consumerId) {
+        this.consumerId = consumerId;
+    }
+}
