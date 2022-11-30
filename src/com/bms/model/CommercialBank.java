cd1c28e0e8ea0fd11fa85aa10009c93cc9a47683
KhedkarAshwini <khedkar.as@northeastern.edu>
2022-11-29 21:36:17 -0500

    creation of commercialbanking class

---
+++ src/com/bms/model/CommercialBank.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bms.model;
+
+/**
+ *
+ * @author ashwini
+ */
+public class CommercialBank {
+    private int entityId;
+    private String entityttype;
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public String getEntityttype() {
+        return entityttype;
+    }
+
+    public void setEntityttype(String entityttype) {
+        this.entityttype = entityttype;
+    }
+    
+    
+}
