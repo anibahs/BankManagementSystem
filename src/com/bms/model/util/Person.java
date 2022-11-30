70aeb52d1b474b6162618f038ae14e3a276edd18
Shabina Singh <anibahs@gmail.com>
2022-11-30 17:08:28 -0500

    added util

---
+++ src/com/bms/model/util/Person.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bms.model.util;
+
+/**
+ *
+ * @author Shabina
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int personId;
+    private String gender;
+    private String address;
+    private int age;
+    private String phoneNumber;
+    private String emailAddress;
+    private char[] password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+}
---
+++ src/com/bms/model/util/PersonDirectory.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bms.model.util;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Shabina
+ */
+public class PersonDirectory {
+    private ArrayList<PersonDirectory> personDirectory;
+
+    public ArrayList<PersonDirectory> getPersonDirectory() {
+        return personDirectory;
+    }
+
+    public void setPersonDirectory(ArrayList<PersonDirectory> personDirectory) {
+        this.personDirectory = personDirectory;
+    }
+}
